057669227e3c448c071d3cdbbeb7b215c18a10c9
Pascal Herms <p.herms@xablu.com>
2021-03-11 16:44:24 +0100

    Updated curl command

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,12 +11,5 @@ if [[ "${GITHUB_EVENT_NAME}" == "pull_request" ]]; then
 fi
 
 curl --location --request POST \
-'https://api.appcenter.ms/v0.1/apps/${INPUT_ORGANIZATION}/${INPUT_APPLICATION}/branches/${INPUT_BRANCH}/builds' \
---header 'X-API-token: ${INPUT_APPCENTERTOKEN}'
-
-# Curl command to call apcenter API
-#curl --location --request POST \
-#'https://api.appcenter.ms/v0.1/apps/p.herms-xablu.com/xablu.newsapp.pascal/branches/sonarqube-test-pascal/builds' \
-#--header 'X-API-Token: 0bc5bb257721aae659855fa5b378e1949eb6421e'
-
-
+"https://api.appcenter.ms/v0.1/apps/${INPUT_ORGANIZATION}/${INPUT_APPLICATION}/branches/${INPUT_BRANCH}/builds" \
+--header "X-API-token: ${INPUT_APPCENTERTOKEN}"
